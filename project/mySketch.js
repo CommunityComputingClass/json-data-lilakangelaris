e778dd882ab9dccde488f38befc7dd85d71d020b
lilakangelaris <125599089+lilakangelaris@users.noreply.github.com>
2025-04-03 22:35:10 +0000

    flower

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,10 +1,22 @@
+let data;
+
+
+function preload() {
+  data = loadJSON("flower.json");
+}
 
 function setup() {
   createCanvas(400, 400);
-  background(200);
+  background(194, 196, 255);
+  
+  let flowers = data.flowers;
  
+  fill(flowers[2].r,flowers[2].g,flowers[2].b);
+  ellipse(150,150,150,150);
+  stroke(0);
+  textSize(22);
+  text(flowers[2].name, 100, 150);
 }
 
-function draw(){
-  
-}
+
+
